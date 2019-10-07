a5c4b43797d8cb714c7676c6fd9f0d93f4de0498
Josh W <jtwong10@gmail.com>
2019-10-06 18:43:35 -0700

    updated to match UML draft

---
+++ trainer_stats.py
@@ -0,0 +1,35 @@
+class TrainerStats:
+    """ Trainer stats class """
+
+    def __init__(self, num_trainers, num_gym_leader, num_regular_trainer, num_movement_type, num_have_partner, num_per_location):
+        """ Constructor for trainer stats """
+        self._num_trainers = num_trainers
+        self._num_gym_leaders = num_gym_leader
+        self._num_regular_trainer = num_regular_trainer
+        self._num_movement_type = num_movement_type
+        self._num_have_partner = num_have_partner
+        self._num_per_location = num_per_location
+
+    def get_num_trainers(self):
+        """ """
+        pass
+
+    def get_num_gym_leader(self):
+        """ """
+        pass
+
+    def get_num_regular_trainer(self):
+        """ """
+        pass
+
+    def get_num_movement_type(self):
+        """ """
+        pass
+
+    def get_num_trainer_have_partner(self):
+        """ """
+        pass
+
+    def get_num_per_location(self):
+        """ """
+        pass
