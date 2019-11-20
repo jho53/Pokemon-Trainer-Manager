741dfe1a6fb2caf81e06e6b1b48df5a3b4a0efc5
Jimmy Ho <jim.eho97@gmail.com>
2019-11-19 22:42:50 -0800

    UML Changes for Ass4

--- trainer_manager_api.py
+++ trainer_manager_api.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 trainer_manager = TrainerManager("./trainer_list.json")
 
+
 # API Methods
 @app.route('/trainermanager/trainers', methods=['POST'])
 def add_trainer():
@@ -16,16 +17,21 @@ def add_trainer():
     content = request.json
     try:
         if content['type'] == 'Regular Trainer':
-            regular_trainer = RegularTrainer(content['name'], content['pokemon_team'], content['trainer_class'],
-                                             content['pokecoins'], content['location'], content['movement_type'],
-                                             content['phone_num'], content['have_partner'])
-            response = app.response_class(response=str(
-                trainer_manager.add(regular_trainer)), status=200)
+            regular_trainer = RegularTrainer(
+                content['name'], content['pokemon_team'],
+                content['trainer_class'], content['pokecoins'],
+                content['location'], content['movement_type'],
+                content['phone_num'], content['have_partner'])
+            response = app.response_class(
+                response=str(trainer_manager.add(regular_trainer)), status=200)
         elif content['type'] == 'Gym Leader':
-            gym_leader = GymLeader(content['name'], content['pokemon_team'], content['trainer_class'],
-                                   content['pokecoins'], content['location'], content['badge'], content['element'], content['prize'])
-            response = app.response_class(response=str(
-                trainer_manager.add(gym_leader)), status=200)
+            gym_leader = GymLeader(content['name'], content['pokemon_team'],
+                                   content['trainer_class'],
+                                   content['pokecoins'], content['location'],
+                                   content['badge'], content['element'],
+                                   content['prize'])
+            response = app.response_class(
+                response=str(trainer_manager.add(gym_leader)), status=200)
         else:
             response = app.response_class(
                 response='Type not supported', status=400)
@@ -40,15 +46,20 @@ def update_trainer(trainer_id):
     content = request.json
     try:
         if content['type'] == 'Regular Trainer':
-            regular_trainer = RegularTrainer(content['name'], content['pokemon_team'], content['trainer_class'],
-                                             content['pokecoins'], content['location'], content['movement_type'],
-                                             content['phone_num'], content['have_partner'])
+            regular_trainer = RegularTrainer(
+                content['name'], content['pokemon_team'],
+                content['trainer_class'], content['pokecoins'],
+                content['location'], content['movement_type'],
+                content['phone_num'], content['have_partner'])
             regular_trainer.id = trainer_id
             trainer_manager.update(regular_trainer)
             response = app.response_class(status=200)
         elif content['type'] == 'Gym Leader':
-            gym_leader = GymLeader(content['name'], content['pokemon_team'], content['trainer_class'],
-                                   content['pokecoins'], content['location'], content['badge'], content['element'], content['prize'])
+            gym_leader = GymLeader(content['name'], content['pokemon_team'],
+                                   content['trainer_class'],
+                                   content['pokecoins'], content['location'],
+                                   content['badge'], content['element'],
+                                   content['prize'])
             gym_leader.id = trainer_id
             trainer_manager.update(gym_leader)
             response = app.response_class(status=200)
@@ -77,8 +88,10 @@ def get_trainer_by_id(trainer_id):
     try:
         result = trainer_manager.get_trainer_by_id(trainer_id)
         if result is not None:
-            response = app.response_class(status=200, response=json.dumps(
-                result.to_dict()), mimetype='application/json')
+            response = app.response_class(
+                status=200,
+                response=json.dumps(result.to_dict()),
+                mimetype='application/json')
         else:
             response = app.response_class(
                 response='ID not found in database', status=404)
@@ -93,7 +106,10 @@ def get_all_trainers():
     trainer_dict_list = []
     for curr_trainer in trainer_manager.get_all():
         trainer_dict_list.append(curr_trainer.to_dict())
-    return app.response_class(status=200, response=json.dumps(trainer_dict_list), mimetype='application/json')
+    return app.response_class(
+        status=200,
+        response=json.dumps(trainer_dict_list),
+        mimetype='application/json')
 
 
 @app.route('/trainermanager/trainers/all/<type>', methods=['GET'])
@@ -111,7 +127,9 @@ def get_all_trainers_by_type(type):
         trainer_dict_list.append(curr_trainer.to_dict())
     try:
         response = app.response_class(
-            status=200, response=json.dumps(trainer_dict_list), mimetype='application/json')
+            status=200,
+            response=json.dumps(trainer_dict_list),
+            mimetype='application/json')
     except Exception as e:
         response = app.response_class(response=str(e), status=400)
     return response
@@ -121,12 +139,16 @@ def get_all_trainers_by_type(type):
 def get_stats():
     '''Get stats for trainer manager'''
     stat = trainer_manager.get_stats()
-    result = ['Number of Trainers: %d' % stat.get_num_trainers(),
-              'Number of Gym Leaders: %d' % stat.get_num_gym_leader(),
-              'Number of Regular Trainers: %d' % stat.get_num_regular_trainer(),
-              'Number of Trainers With Partners: %d' % stat.get_num_trainer_have_partner(),
-              ('Trainers per location: {}').format(stat.get_num_per_location())]
-    return app.response_class(status=200, response=json.dumps(result), mimetype='application/json')
+    result = [
+        'Number of Trainers: %d' % stat.get_num_trainers(),
+        'Number of Gym Leaders: %d' % stat.get_num_gym_leader(),
+        'Number of Regular Trainers: %d' % stat.get_num_regular_trainer(),
+        'Number of Trainers With Partners: %d' %
+        stat.get_num_trainer_have_partner(),
+        ('Trainers per location: {}').format(stat.get_num_per_location())
+    ]
+    return app.response_class(
+        status=200, response=json.dumps(result), mimetype='application/json')
 
 
 if __name__ == "__main__":
